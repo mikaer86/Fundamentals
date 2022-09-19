1f415a7984bdc17512b24dadd66bcc87c39f1b1c
mikaer86 <mikaeleriksson123@gmail.com>
2022-09-19 09:33:15 +0200

    javascript file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const targetImg = document.getElementById('heart');
+const targetTag = document.getElementById('number');
+
+function like(targetImg, targetTag) {
+
+    if(onclick === heart){
+
+
+
+
+    }
+
+}
