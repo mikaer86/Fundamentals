042293a75d9f82de0b7cb43f93fa32c0fa696899
mikaer86 <mikaeleriksson123@gmail.com>
2022-09-28 14:25:36 +0200

    taken away the like button functionality

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
 // ################### Like Button ###################
 
-heartButtons = document.getElementsByClassName("svg-heart");
-console.dir(heartButtons);
-
-for (let element of heartButtons) {
-  //set up eventlisterners
-
-  element.addEventListener("click", (e) => {
-    //use e or element
-    //create a 'red-heart' css class
-    //then use this code instead of "coloring code"
-    //element.classList.toggle('red-heart')
-
-    e.target.style.stroke = "red";
-    e.target.style.fill = "red";
-  });
-  console.log(element);
-}
-
-function like() {}
-
-//const likeCount = document.getElementById('number');
-
 //  ################# Navigation ################
 
 const navMobile = document.querySelector(".hamburger");
