474546f72dc9f26af454e278d544e69b11c7e789
mikaer86 <mikaeleriksson123@gmail.com>
2023-05-21 23:51:21 +0200

    ScrollTrigger.batch animation

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,13 @@
-import gsap from 'gsap';
+gsap.defaults({ease: "power4"});
+gsap.set(".film-img-section", {y: 100});
 
+ScrollTrigger.batch(".film-img-section", {
+  
+  onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.18, grid: [1, 3]}, overwrite: true}),
+  onLeave: batch => gsap.set(batch, {opacity: 0, y: -100, overwrite: true}),
+  onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.18, overwrite: true}),
+  onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
+  // you can also define things like start, end, etc.
+});
 
-const { default: gsap } = require('gsap');
-
-gsap.set(".flex-wrapper", {scale:0.7});
-
-gsap.timeline()
-        .from(".flex-wrapper", {duration: 1, opacity:0})
+ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".film-img-section", {y: 0}));
